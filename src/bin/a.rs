4e207b5afdc9e0e2efa008d755b9195954269a81
mcbarinov <mcbarinov@proton.me>
2024-08-29 12:38:09 +0300

    a base

--- src/bin/a.rs
+++ src/bin/a.rs
@@ -16,21 +16,26 @@ enum Command {
 
     /// Install Caddy via local.mm.install_caddy
     Caddy { host: String, domain: Option<String> },
+
+    /// Setup Ubuntu via local.mm.bootstrap_ubuntu
+    Base { host: String },
 }
 
 fn main() {
     match Cli::parse().command {
-        Command::Docker { host } => {
-            let cmd = format!("ANSIBLE_LOAD_CALLBACK_PLUGINS=true ANSIBLE_STDOUT_CALLBACK=yaml ansible -i {host}, all -m include_role -a name=local.mm.install_docker");
-            shell(&cmd)
-        }
-
+        Command::Docker { host } => shell(&ansible_command(host, "local.mm.install_docker")),
         Command::Caddy { host, domain } => {
-            let mut cmd = format!("ANSIBLE_LOAD_CALLBACK_PLUGINS=true ANSIBLE_STDOUT_CALLBACK=yaml ansible -i {host}, all -m include_role -a name=local.mm.install_caddy");
+            let mut cmd = ansible_command(host, "local.mm.install_caddy");
             if domain.is_some() {
                 cmd += &format!(" -e domain={}", domain.unwrap());
             }
             shell(&cmd)
         }
+        Command::Base { host } => shell(&ansible_command(host, "local.mm.bootstrap_ubuntu")),
     }
+}
+
+fn ansible_command(host: String, role: &str) -> String {
+    let env = "ANSIBLE_LOAD_CALLBACK_PLUGINS=true ANSIBLE_STDOUT_CALLBACK=yaml";
+    format!("{env} ansible -i {host}, all -m include_role -a name={role}")
 }
