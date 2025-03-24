171e6ee31159d78339a315e13d917af81bcbae61
mcbarinov <mcbarinov@proton.me>
2025-03-24 17:25:39 +0300

    p: use uv

--- src/mm_dev/p.py
+++ src/mm_dev/p.py
@@ -10,39 +10,24 @@ from mm_dev._common import create_app
 app = create_app(multi_command=True)
 
 
-@app.command(name="o", help="pip list -o")
+@app.command(name="o", help="uv pip list --outdated")
 def pip_list_outdated() -> None:
-    run_command("pip list -o", capture_output=False)
+    run_command("uv pip list -o", capture_output=False)
 
 
-@app.command(name="l", help="pip list")
+@app.command(name="l", help="uv pip list")
 def pip_list() -> None:
-    run_command("pip list", capture_output=False)
+    run_command("uv pip list", capture_output=False)
 
 
-@app.command(name="u", help="pip install -U pip setuptools wheel")
-def update_pip() -> None:
-    run_command("pip install -U pip setuptools wheel", capture_output=False)
-
-
-@app.command(name="i", help="install packages or project (setup.py or requirements.txt)")
+@app.command(name="i", help="install packages or requirements.txt")
 def install(packages: Optional[str] = Argument(None)) -> None:  # noqa: UP007
     if not os.getenv("VIRTUAL_ENV"):
         fatal("venv is not activated")
-
     if packages:
-        run_command(f"pip install {packages}", capture_output=False)
+        run_command(f"uv pip install {packages}", capture_output=False)
         return
-
-    if Path("setup.py").is_file():
-        run_command("pip install -Ue .[dev]", capture_output=False)
-        return
-
-    if Path("requirements.txt").is_file():
-        run_command("pip install -Ur requirements.txt", capture_output=False)
-        return
-
-    run_command("pdm install", capture_output=False)
+    run_command("uv pip install -Ur requirements.txt", capture_output=False)
 
 
 @app.command(name="v", help="create .venv")
@@ -60,8 +45,8 @@ def uninstall() -> None:
     if not os.getenv("VIRTUAL_ENV"):
         fatal("venv is not activated")
 
-    run_command("pip list --format freeze -e | xargs pip uninstall -y", capture_output=False)
-    run_command("pip freeze | xargs pip uninstall -y", capture_output=False)
+    run_command("uv pip list --format freeze -e | xargs uv pip uninstall -y", capture_output=False)
+    run_command("uv pip freeze | xargs pip uninstall", capture_output=False)
 
 
 @app.command(name="c", help="uv cache clean {package}")
